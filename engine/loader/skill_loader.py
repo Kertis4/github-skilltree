3a70d2a76ae115a7877e604400d7bc7ee0e28bd7
Michael Olawoye <t-molawoye@microsoft.com>
2026-06-24 19:46:55 +0100

    finished skill mapping from user skills to canonical skills, and skill loader

--- engine/loader/skill_loader.py
+++
@@ -1,9 +0,0 @@
-# Loads the json files output from the parsers that read the repo. Outputs the json as dicts and lists
-import json
-
-def read_file(file) -> dict[str, str]:
-    data = {}
-    with open(file, 'r') as f:
-        data = json.load(file)
-
-    return data
--- engine/loader/userskills_loader.py
+++
@@ -1,5 +0,0 @@
-# Turns user skills (from json skill loader) into canonical skills
-
-
-def convert_user_skills_to_canonical_skills(user_skills: list[dict], canonical_skills: list[str]):
-    pass
